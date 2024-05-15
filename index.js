8868f4968eb4ecdbceff9c8b8fab31ca25dd012b
Faisal778 <62201274+Faisal778@users.noreply.github.com>
2024-05-15 14:58:59 +0600

    get query and res created to find single data

--- index.js
+++ index.js
@@ -1,58 +1,64 @@
-const express = require ('express')
-const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config()
-const port = process.env.PORT || 5000
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
 
-const app = express()
+const app = express();
 
 const corsOptions = {
-    origin: ['http://localhost:5173'],
-    Credentials: true,
-    optionSuccessStatus: 200,
-}
-app.use(cors({corsOptions}))
-app.use(express.json())
+  origin: ["http://localhost:5173"],
+  Credentials: true,
+  optionSuccessStatus: 200,
+};
+app.use(cors({ corsOptions }));
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zedvr4o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    }
-  });
-  async function run() {
-    try {
-      // Connect the client to the server	(optional starting in v4.7)
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
 
-const jobsCollection = client.db('proOpportunity').collection('jobs')
-      // Get data from database
+    const jobsCollection = client.db("proOpportunity").collection("jobs");
 
-      app.get('/jobs', async(req, res) => {
-            const result = await jobsCollection.find().toArray()
 
-            res.send(result)
-      })
-      // Send a ping to confirm a successful connection
-      await client.db("admin").command({ ping: 1 });
-      console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-      // Ensures that the client will close when you finish/error
+    // Get all data from database
 
-    }
+    app.get("/jobs", async (req, res) => {
+      const result = await jobsCollection.find().toArray()
+
+      res.send(result)
+    });
+
+
+    //get single data from database using id
+
+    app.get('/job/:id', async (req, res)=>{
+        const id = req.params.id
+        const query = {_id: new ObjectId(id)}
+        const result = await jobsCollection.findOne(query)
+        res.send(result)
+    })
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
   }
-  run().catch(console.dir);
+}
+run().catch(console.dir);
 
-app.get('/', (req, res)=>{
-    res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-
-
-
-app.listen(port, ()=>
-    console.log(`server running on port ${port}`)
-    )
+app.listen(port, () => console.log(`server running on port ${port}`));
